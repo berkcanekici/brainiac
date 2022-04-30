e272aae63c2d865183e68b41533f43f3374081e1
yesimyalc <y.yalcin072@gmail.com>
2022-04-30 18:18:03 +0300

    Test Start Page Almost Done

--- application/BrainiacApp/BrainiacApp/Test.xaml.cs
+++ application/BrainiacApp/BrainiacApp/Test.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
+using System.Globalization;
+
 
 namespace BrainiacApp
 {
@@ -20,10 +23,40 @@ namespace BrainiacApp
     /// </summary>
     public partial class Test : Page
     {
+        private String userName;
         public Test()
         {
             InitializeComponent();
             this.FontFamily = new FontFamily("Alata");
         }
+
+        private void NameTextboxClicked(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            tb.Text = string.Empty;
+            tb.GotFocus -= NameTextboxClicked;
+        }
+
+        private void EnglishSelected(object sender, RoutedEventArgs e)
+        {
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("en-US");
+        }
+
+        private void TurkishSelected(object sender, RoutedEventArgs e)
+        {
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("tr-TR");
+        }
+
+        private void OnStart(object sender, RoutedEventArgs e)
+        {
+            if(NameEnterBox.Text!=String.Empty)
+            {
+                userName = NameEnterBox.Text;
+                FirstPage.Visibility = Visibility.Collapsed;
+                LeftBar.Visibility = Visibility.Visible;
+                QuestionFrame.Visibility = Visibility.Visible;
+            }
+        }
+
     }
 }
--- application/BrainiacApp/BrainiacApp/obj/Debug/BrainiacApp_Content.g.i.cs
+++ application/BrainiacApp/BrainiacApp/obj/Debug/BrainiacApp_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/gonogo.jpeg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/turkish.png")]
 
 
