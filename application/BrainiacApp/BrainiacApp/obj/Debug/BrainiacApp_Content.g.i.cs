07bacc1eee3bd29ae90a08240c4cc4c67ace33e6
yesimyalc <y.yalcin072@gmail.com>
2022-04-30 16:57:50 +0300

    Help Center Page Done

--- application/BrainiacApp/BrainiacApp/obj/Debug/BrainiacApp_Content.g.i.cs
+++ application/BrainiacApp/BrainiacApp/obj/Debug/BrainiacApp_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/response.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/gonogo.jpeg")]
 
 
