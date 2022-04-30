018a63f9ac27354d36c8b0576a172274e0f4bb16
yesimyalc <y.yalcin072@gmail.com>
2022-04-30 19:49:12 +0300

    Test Page Continue

--- application/BrainiacApp/BrainiacApp/obj/Debug/BrainiacApp_Content.g.i.cs
+++ application/BrainiacApp/BrainiacApp/obj/Debug/BrainiacApp_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/turkish.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icons/pause.png")]
 
 
